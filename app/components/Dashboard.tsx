455842e00795de9806067943c151d72a55a0dc37
Milton-Luna <miltonluna.m.16@gmail.com>
2026-06-03 22:47:02 -0500

    implementacion filterBar

--- app/components/Dashboard.tsx
+++ app/components/Dashboard.tsx
@@ -1,15 +1,17 @@
 import { useState } from "react";
 import type { TaskFilters } from "../types";
+import { useTaskStore } from "../store/useTaskStore";
 import { defaultFilters } from "../utils/filterTasks";
 import { FilterBar } from "./FilterBar";
 import { TaskBoard } from "./TaskBoard";
 
 export function Dashboard() {
   const [filters, setFilters] = useState<TaskFilters>(defaultFilters);
+  const tasks = useTaskStore((s) => s.tasks);
 
   return (
     <div className="flex flex-col gap-4 p-4">
-      <FilterBar filters={filters} onChange={setFilters} />
+      <FilterBar filters={filters} onChange={setFilters} tasks={tasks} />
       <TaskBoard filters={filters} />
     </div>
   );
